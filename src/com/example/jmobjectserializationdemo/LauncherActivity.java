f1e53c566db4defcfa5a78e12afb0fd5e2c3c9ae
Jatin_Malwal_Inox <jatin.malwal@inoxapps.com>
2013-10-01 19:17:08 +0530

    Saving objects to external files

--- src/com/example/jmobjectserializationdemo/LauncherActivity.java
+++ src/com/example/jmobjectserializationdemo/LauncherActivity.java
@@ -1,5 +1,6 @@
 package com.example.jmobjectserializationdemo;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -11,6 +12,7 @@ import java.io.StreamCorruptedException;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Menu;
 import android.view.View;
 import android.widget.TextView;
@@ -19,6 +21,11 @@ import android.widget.Toast;
 public class LauncherActivity extends Activity {
 
 	Mario mario;
+	String root;
+	private File myDir;
+	private File file;
+	public static final String FILE_NAME = "mario.obj"; // File name could be
+														// anything
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +33,21 @@ public class LauncherActivity extends Activity {
 		setContentView(R.layout.activity_launcher);
 		mario = new Mario();
 		setText();
+
+		root = Environment.getExternalStorageDirectory().toString();
+		// root = Environment.getRootDirectory().toString(); //For getting the
+		// root path
+		Toast.makeText(this, root, Toast.LENGTH_SHORT).show();
+		// myDir = new File(root + "/.mario"); //Making hidden folder
+		myDir = new File(root + "/mario");
+		myDir.mkdirs();
+
+		file = new File(myDir, FILE_NAME);
+
+		// Deleting the file
+		// if (file.exists()) {
+		// file.delete();
+		// }
 
 	}
 
@@ -37,13 +59,13 @@ public class LauncherActivity extends Activity {
 			mario.setIsPowerUpActivated(true);
 			mario.setToatlLife(4);
 			try {
-				FileOutputStream fileOutputStream = this.openFileOutput("Mario", Context.MODE_PRIVATE);
-
+				FileOutputStream fileOutputStream = new FileOutputStream(file);
+				// FileOutputStream fileOutputStream =
+				// this.openFileOutput("Mario",Context.MODE_PRIVATE);
 				ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
-
 				objectOutputStream.writeObject(mario);
-
-				Toast.makeText(this, "Serialized", Toast.LENGTH_LONG).show();
+				objectOutputStream.close();
+				Toast.makeText(this, "Serialized", Toast.LENGTH_SHORT).show();
 
 			} catch (FileNotFoundException e) {
 				// TODO Auto-generated catch block
@@ -56,19 +78,20 @@ public class LauncherActivity extends Activity {
 		case R.id.buttonD:
 
 			try {
-				FileInputStream fileInputStream = this.openFileInput("Mario");
-
+				// FileInputStream fileInputStream =
+				// this.openFileInput("Mario");
+				FileInputStream fileInputStream = new FileInputStream(file);
 				ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
 
 				mario = (Mario) objectInputStream.readObject();
-
-				Toast.makeText(this, "Deserialized", Toast.LENGTH_LONG).show();
+				objectInputStream.close();
+				Toast.makeText(this, "Deserialized", Toast.LENGTH_SHORT).show();
 
 			} catch (ClassNotFoundException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
+				Toast.makeText(this, "File not found", Toast.LENGTH_SHORT).show();
 				e.printStackTrace();
 			} catch (StreamCorruptedException e) {
 				// TODO Auto-generated catch block
